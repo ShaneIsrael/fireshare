02c4cceedf70050a5b8c8aa64d90272c3a8d8515
Shane Israel <shanemisrael@gmail.com>
2022-05-24 17:42:18 -0600

    need to actually include the port in the url if a port is being used

--- app/client/src/common/utils.js
+++ app/client/src/common/utils.js
@@ -8,16 +8,18 @@ export const getServedBy = () => {
 }
 
 export const getUrl = () => {
+  const portWithColon = window.location.port ? `:${window.location.port}` : ''
   return window.location.port === 3000 ||
     (window.location.hostname.indexOf('localhost') >= 0 && window.location.port !== '')
     ? 'http://localhost:5000'
-    : `${window.location.protocol}//${window.location.hostname}`
+    : `${window.location.protocol}//${window.location.hostname}${portWithColon}`
 }
 export const getPublicWatchUrl = () => {
+  const portWithColon = window.location.port ? `:${window.location.port}` : ''
   return window.location.port === 3000 ||
     (window.location.hostname.indexOf('localhost') >= 0 && window.location.port !== '')
     ? `http://localhost:${window.location.port}/#/w/`
-    : `${window.location.protocol}//${window.location.hostname}/#/w/`
+    : `${window.location.protocol}//${window.location.hostname}${portWithColon}/#/w/`
 }
 
 export const useDebounce = (value, delay) => {
