d38d39a6ff9c3768591c4c080e322671f6f0c283
Shane Israel <shanemisrael@gmail.com>
2022-05-24 17:35:17 -0600

    use or not and

--- app/client/src/common/utils.js
+++ app/client/src/common/utils.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
 export const getServedBy = () => {
-  return window.location.port === 3000 && window.location.hostname.indexOf('localhost') >= 0 ? 'flask' : 'nginx'
+  return window.location.port === 3000 ||
+    (window.location.hostname.indexOf('localhost') >= 0 && window.location.port !== '')
+    ? 'flask'
+    : 'nginx'
 }
 
 export const getUrl = () => {
-  return window.location.port === 3000 && window.location.hostname.indexOf('localhost') >= 0
+  return window.location.port === 3000 ||
+    (window.location.hostname.indexOf('localhost') >= 0 && window.location.port !== '')
     ? 'http://localhost:5000'
     : `${window.location.protocol}//${window.location.hostname}`
 }
 export const getPublicWatchUrl = () => {
-  return window.location.port === 3000 && window.location.hostname.indexOf('localhost') >= 0
-    ? 'http://localhost:3000/#/w/'
+  return window.location.port === 3000 ||
+    (window.location.hostname.indexOf('localhost') >= 0 && window.location.port !== '')
+    ? `http://localhost:${window.location.port}/#/w/`
     : `${window.location.protocol}//${window.location.hostname}/#/w/`
 }
 
