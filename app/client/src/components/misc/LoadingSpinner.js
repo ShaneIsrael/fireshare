5271868151e07b9085a013aab1f848e3089bf6e6
Shane Israel <shanemisrael@gmail.com>
2022-05-25 11:31:26 -0600

    fancier loading progress

--- app/client/src/components/misc/LoadingSpinner.js
+++ app/client/src/components/misc/LoadingSpinner.js
@@ -1,6 +1,42 @@
 import * as React from 'react'
-import CircularProgress from '@mui/material/CircularProgress'
+import { styled } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import CircularProgress, { circularProgressClasses } from '@mui/material/CircularProgress'
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress'
+
+function LoadingProgress({ size, ...rest }) {
+  return (
+    <Box sx={{ position: 'relative' }}>
+      <CircularProgress
+        variant="determinate"
+        sx={{
+          color: (theme) => theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+        }}
+        size={size ? size : 75}
+        thickness={4}
+        {...rest}
+        value={100}
+      />
+      <CircularProgress
+        variant="indeterminate"
+        disableShrink
+        sx={{
+          color: (theme) => (theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8'),
+          animationDuration: '550ms',
+          position: 'absolute',
+          left: 0,
+          [`& .${circularProgressClasses.circle}`]: {
+            strokeLinecap: 'round',
+          },
+        }}
+        size={size ? size : 75}
+        thickness={4}
+        {...rest}
+      />
+    </Box>
+  )
+}
 
 export default function LoadingSpinner({ size }) {
-  return <CircularProgress disableShrink size={size ? size : 75} />
+  return <LoadingProgress size={size} />
 }
