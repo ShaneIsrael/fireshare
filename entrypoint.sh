49578cbb08bcf46dfdd72603757fbb35509c159d
Shane Israel <shanemisrael@gmail.com>
2022-05-26 15:57:16 -0600

    adjust gunicorn workers and threads, move nginx aio threads to server block

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 nginx -g 'daemon on;'
-gunicorn --bind=127.0.0.1:5000 "fireshare:create_app()" --workers 1 --threads 12
+gunicorn --bind=127.0.0.1:5000 "fireshare:create_app()" --workers=3 --threads=3
