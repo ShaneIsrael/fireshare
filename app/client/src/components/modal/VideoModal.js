a47699d0befc13d7fd9f8fcc53c97cd908bbcade
Shane Israel <shanemisrael@gmail.com>
2022-05-29 19:27:00 -0600

    allow title change in the preview modal, update look and feel of preview modal controls

--- app/client/src/components/modal/VideoModal.js
+++ app/client/src/components/modal/VideoModal.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Box, Button, ButtonGroup, Grid, Modal, Typography } from '@mui/material'
+import { Box, Button, ButtonGroup, Grid, Modal, TextField, Typography } from '@mui/material'
 import LinkIcon from '@mui/icons-material/Link'
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
 import ShuffleIcon from '@mui/icons-material/Shuffle'
 import ReactPlayer from 'react-player'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { getPublicWatchUrl, getServedBy, getUrl } from '../../common/utils'
+import { getPublicWatchUrl, getServedBy, getUrl, useDebounce } from '../../common/utils'
 import { VideoService } from '../../services'
 import SnackbarAlert from '../alert/SnackbarAlert'
 
@@ -15,8 +15,11 @@ const SERVED_BY = getServedBy()
 
 const VideoModal = ({ open, onClose, video, feedView }) => {
   const [vid, setVideo] = React.useState(video)
+  const [updatedTitle, setUpdatedTitle] = React.useState(null)
   const [alert, setAlert] = React.useState({ open: false })
   const playerRef = React.useRef(null)
+  const debouncedTitle = useDebounce(updatedTitle, 1500)
+  const title = video?.info?.title
 
   const getRandomVideo = async () => {
     try {
@@ -36,6 +39,28 @@ const VideoModal = ({ open, onClose, video, feedView }) => {
       window.open(`${PURL}${vid.video_id}`, '_blank')
     }
   }
+
+  React.useEffect(() => {
+    async function update() {
+      try {
+        await VideoService.updateTitle(video.video_id, debouncedTitle)
+        setAlert({
+          type: 'success',
+          message: 'Title Updated',
+          open: true,
+        })
+      } catch (err) {
+        setAlert({
+          type: 'error',
+          message: 'An error occurred trying to update the title',
+          open: true,
+        })
+      }
+    }
+    if (debouncedTitle && debouncedTitle !== title) {
+      update()
+    }
+  }, [debouncedTitle, title, video, setAlert])
 
   if (!vid) return null
 
@@ -60,11 +85,11 @@ const VideoModal = ({ open, onClose, video, feedView }) => {
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            width: '80%',
+            width: '90%',
           }}
         >
           <Grid container justifyContent="center">
-            <Grid item xs={12} sx={{ background: 'rgba(0, 0, 0, 1)' }}>
+            {/* <Grid item xs={12} sx={{ background: 'rgba(0, 0, 0, 1)' }}>
               <Typography
                 align="center"
                 noWrap
@@ -78,7 +103,7 @@ const VideoModal = ({ open, onClose, video, feedView }) => {
               >
                 {vid?.info.title}
               </Typography>
-            </Grid>
+            </Grid> */}
             <Grid item xs={12}>
               <ReactPlayer
                 ref={playerRef}
@@ -94,10 +119,20 @@ const VideoModal = ({ open, onClose, video, feedView }) => {
               />
             </Grid>
             <Grid item>
-              <ButtonGroup variant="contained">
+              <ButtonGroup variant="contained" sx={{ minWidth: 525 }}>
                 <Button startIcon={<ShuffleIcon />} onClick={getRandomVideo}>
                   Play Random
                 </Button>
+                <TextField
+                  sx={{
+                    textAlign: 'center',
+                    '& .MuiOutlinedInput-root': { borderRadius: 0 },
+                    background: 'rgba(50, 50, 50, 0.9)',
+                  }}
+                  size="small"
+                  defaultValue={updatedTitle || title}
+                  onChange={(e) => setUpdatedTitle(e.target.value)}
+                />
                 <CopyToClipboard text={`${PURL}${vid.video_id}`}>
                   <Button
                     onMouseDown={handleMouseDown}
