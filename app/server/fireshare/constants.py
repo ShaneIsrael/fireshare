599ef6ceb909160d63050df0a51e4b644ed3d37b
Shane Israel <shanemisrael@gmail.com>
2022-06-25 11:59:51 -0600

    update version, add upload configuration options

--- app/server/fireshare/constants.py
+++ app/server/fireshare/constants.py
@@ -3,8 +3,11 @@ DEFAULT_CONFIG = {
     "video_defaults": {
       "private": True
     },
+    "allow_public_upload": False,
   },
   "ui_config": {
-    "shareable_link_domain": ""
+    "shareable_link_domain": "",
+    "show_public_upload": False,
+    "show_admin_upload": True,
   }
 }
