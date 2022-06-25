17b5f88781b9c6822385fa668f5b8c1a9d38c0c4
Shane Israel <shanemisrael@gmail.com>
2022-06-25 10:15:24 -0600

    only render visible list items

--- app/client/src/components/admin/VideoListItem.js
+++ app/client/src/components/admin/VideoListItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Grid, IconButton, InputAdornment, Paper, TextField, Typography } from '@mui/material'
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip'
 import PlayCircleIcon from '@mui/icons-material/PlayCircle'
@@ -14,6 +14,7 @@ import { VideoService } from '../../services'
 import UpdateDetailsModal from '../modal/UpdateDetailsModal'
 import styled from '@emotion/styled'
 import _ from 'lodash'
+import { useIsVisible } from 'react-is-visible'
 
 const URL = getPublicWatchUrl()
 
@@ -110,111 +111,128 @@ const VideoListItem = ({ video, openVideoHandler, alertHandler, authenticated, d
     }
   }
 
+  const nodeRef = useRef()
+  const isVisible = useIsVisible(nodeRef)
+
   return (
-    <Paper square sx={{ height: 70, bgcolor: '#0b132b', borderBottom: '1px solid #046595' }}>
-      <UpdateDetailsModal
-        open={detailsModalOpen}
-        close={handleDetailsModalClose}
-        videoId={video.video_id}
-        currentTitle={title || ''}
-        currentDescription={description || ''}
-        alertHandler={alertHandler}
-      />
-      <Grid container direction="column" sx={{ width: '100%', height: '100%' }}>
-        <Grid container sx={{ width: 25, height: '100%', pl: 2 }} justifyContent="center" alignItems="center">
-          <Grid item>
-            <CopyToClipboard text={`${URL}${video.video_id}`}>
-              <IconButton
-                aria-label="play video"
-                sx={{ width: 30, height: 30 }}
-                onMouseDown={handleMouseDown}
-                onClick={() =>
-                  alertHandler({
-                    open: true,
-                    message: 'Link copied to clipboard',
-                    type: 'info',
-                  })
-                }
-              >
-                <LinkIcon sx={{ width: 25, height: 25 }} color="primary" />
-              </IconButton>
-            </CopyToClipboard>
-          </Grid>
-        </Grid>
-        {authenticated && (
-          <Grid container sx={{ width: 25, height: '100%', pl: 2 }} justifyContent="center" alignItems="center">
-            <Tooltip
-              title="Toggle visibility on your public feed."
-              placement="top"
-              enterDelay={1000}
-              TransitionComponent={Zoom}
+    <div ref={nodeRef}>
+      {!isVisible && (
+        <div
+          // calculate the rendered cards height based on the video dimesions and our css styling heights
+          style={{
+            width: '100%',
+            background: '#000e393b',
+            height: 70,
+          }}
+        />
+      )}
+      {isVisible && (
+        <Paper square sx={{ height: 70, bgcolor: '#0b132b', borderBottom: '1px solid #046595' }}>
+          <UpdateDetailsModal
+            open={detailsModalOpen}
+            close={handleDetailsModalClose}
+            videoId={video.video_id}
+            currentTitle={title || ''}
+            currentDescription={description || ''}
+            alertHandler={alertHandler}
+          />
+          <Grid container direction="column" sx={{ width: '100%', height: '100%' }}>
+            <Grid container sx={{ width: 25, height: '100%', pl: 2 }} justifyContent="center" alignItems="center">
+              <Grid item>
+                <CopyToClipboard text={`${URL}${video.video_id}`}>
+                  <IconButton
+                    aria-label="play video"
+                    sx={{ width: 30, height: 30 }}
+                    onMouseDown={handleMouseDown}
+                    onClick={() =>
+                      alertHandler({
+                        open: true,
+                        message: 'Link copied to clipboard',
+                        type: 'info',
+                      })
+                    }
+                  >
+                    <LinkIcon sx={{ width: 25, height: 25 }} color="primary" />
+                  </IconButton>
+                </CopyToClipboard>
+              </Grid>
+            </Grid>
+            {authenticated && (
+              <Grid container sx={{ width: 25, height: '100%', pl: 2 }} justifyContent="center" alignItems="center">
+                <Tooltip
+                  title="Toggle visibility on your public feed."
+                  placement="top"
+                  enterDelay={1000}
+                  TransitionComponent={Zoom}
+                >
+                  <IconButton
+                    sx={{
+                      color: privateView ? 'red' : '#2684FF',
+                    }}
+                    onClick={handlePrivacyChange}
+                    edge="end"
+                  >
+                    {privateView ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            )}
+            <Grid
+              container
+              sx={{ width: `calc(100% - (75px + ${authenticated ? 25 : 0}px))`, height: '100%', pl: 3 }}
+              alignItems="center"
             >
-              <IconButton
-                sx={{
-                  color: privateView ? 'red' : '#2684FF',
-                }}
-                onClick={handlePrivacyChange}
-                edge="end"
-              >
-                {privateView ? <VisibilityOffIcon /> : <VisibilityIcon />}
-              </IconButton>
-            </Tooltip>
+              <Grid item xs>
+                <TextField
+                  fullWidth
+                  size="small"
+                  value={updatedTitle !== null ? updatedTitle : title}
+                  disabled={!authenticated}
+                  onChange={(e) => authenticated && setUpdatedTitle(e.target.value)}
+                  sx={{ '& .MuiOutlinedInput-root': { borderRadius: 0 } }}
+                  InputProps={{
+                    startAdornment: authenticated && (
+                      <InputAdornment position="start">
+                        <IconButton size="small" onClick={() => setDetailsModalOpen(true)} edge="start">
+                          <EditIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid item sx={{ pl: 1, pr: 1 }}>
+                <Typography sx={{ fontWeight: 400, fontSize: 20, fontFamily: 'monospace' }}>
+                  {`${video.view_count} ${video.view_count === 1 ? 'View' : 'Views'}`}
+                </Typography>
+              </Grid>
+              <Grid item sx={{ pl: 1, pr: 1 }}>
+                <Typography sx={{ fontWeight: 400, fontSize: 20, fontFamily: 'monospace' }}>
+                  {toHHMMSS(video.info.duration)}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container sx={{ width: 50, height: '100%' }} justifyContent="center" alignItems="center">
+              {!video.available ? (
+                <LightTooltip title="File Missing">
+                  <IconButton aria-label="play video" sx={{ width: 50, height: 50, color: 'red' }}>
+                    <ErrorIcon sx={{ width: 40, height: 40 }} />
+                  </IconButton>
+                </LightTooltip>
+              ) : (
+                <IconButton
+                  aria-label="play video"
+                  sx={{ width: 50, height: 50 }}
+                  onClick={() => openVideoHandler(video.video_id)}
+                >
+                  <PlayCircleIcon sx={{ width: 40, height: 40 }} color="primary" />
+                </IconButton>
+              )}
+            </Grid>
           </Grid>
-        )}
-        <Grid
-          container
-          sx={{ width: `calc(100% - (75px + ${authenticated ? 25 : 0}px))`, height: '100%', pl: 3 }}
-          alignItems="center"
-        >
-          <Grid item xs>
-            <TextField
-              fullWidth
-              size="small"
-              value={updatedTitle !== null ? updatedTitle : title}
-              disabled={!authenticated}
-              onChange={(e) => authenticated && setUpdatedTitle(e.target.value)}
-              sx={{ '& .MuiOutlinedInput-root': { borderRadius: 0 } }}
-              InputProps={{
-                startAdornment: authenticated && (
-                  <InputAdornment position="start">
-                    <IconButton size="small" onClick={() => setDetailsModalOpen(true)} edge="start">
-                      <EditIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Grid>
-          <Grid item sx={{ pl: 1, pr: 1 }}>
-            <Typography sx={{ fontWeight: 400, fontSize: 20, fontFamily: 'monospace' }}>
-              {`${video.view_count} ${video.view_count === 1 ? 'View' : 'Views'}`}
-            </Typography>
-          </Grid>
-          <Grid item sx={{ pl: 1, pr: 1 }}>
-            <Typography sx={{ fontWeight: 400, fontSize: 20, fontFamily: 'monospace' }}>
-              {toHHMMSS(video.info.duration)}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid container sx={{ width: 50, height: '100%' }} justifyContent="center" alignItems="center">
-          {!video.available ? (
-            <LightTooltip title="File Missing">
-              <IconButton aria-label="play video" sx={{ width: 50, height: 50, color: 'red' }}>
-                <ErrorIcon sx={{ width: 40, height: 40 }} />
-              </IconButton>
-            </LightTooltip>
-          ) : (
-            <IconButton
-              aria-label="play video"
-              sx={{ width: 50, height: 50 }}
-              onClick={() => openVideoHandler(video.video_id)}
-            >
-              <PlayCircleIcon sx={{ width: 40, height: 40 }} color="primary" />
-            </IconButton>
-          )}
-        </Grid>
-      </Grid>
-    </Paper>
+        </Paper>
+      )}
+    </div>
   )
 }
 
