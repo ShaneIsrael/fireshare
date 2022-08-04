5aa8f4c51dcdaded79bc17299cf39c8f269a43fb
Shane Israel <shanemisrael@gmail.com>
2022-08-03 21:12:42 -0600

    run nginx as root, then change to appuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #/bin/bash
 
+nginx -g 'daemon on;'
+
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
+useradd appuser || true
+
 groupmod -o -g "$PGID" appuser
 usermod -o -u "$PUID" appuser
+
+su appuser
 
 echo '
 -------------------------------------'
@@ -18,7 +24,7 @@ chown appuser:appuser $DATA_DIRECTORY
 chown appuser:appuser $VIDEO_DIRECTORY
 chown appuser:appuser $PROCESSED_DIRECTORY
 
-su appuser
+
 
 # Remove any lockfiles on startup
 rm $DATA_DIRECTORY/*.lock 2> /dev/null
@@ -26,6 +32,6 @@ rm $DATA_DIRECTORY/*.lock 2> /dev/null
 # Remove job db on start
 rm /jobs.sqlite
 
-nginx -g 'daemon on;'
+
 flask db upgrade
 gunicorn --bind=127.0.0.1:5000 "fireshare:create_app(init_schedule=True)" --workers 3 --threads 3 --preload
