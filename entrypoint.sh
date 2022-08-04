fa840c94bac4bd20271c8a3b515ec88f8755ef46
Shane Israel <shanemisrael@gmail.com>
2022-08-03 20:35:22 -0600

    do not use root user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,28 @@
 #/bin/bash
 
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+
+groupmod -o -g "$PGID" appuser
+usermod -o -u "$PUID" appuser
+
+echo '
+-------------------------------------'
+echo '
+User uid:    $(id -u appuser)
+User gid:    $(id -g appuser)
+-------------------------------------
+'
+
+chown appuser:appuser $DATA_DIRECTORY
+chown appuser:appuser $VIDEO_DIRECTORY
+chown appuser:appuser $PROCESSED_DIRECTORY
+
+su appuser
+
 # Remove any lockfiles on startup
-rm $DATA_DIRECTORY/*.lock
+rm $DATA_DIRECTORY/*.lock 2> /dev/null
+
 # Remove job db on start
 rm /jobs.sqlite
 
