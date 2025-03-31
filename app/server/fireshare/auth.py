a5b0f9ad238097ce33ec38415b28ab122cb46d0c
msmcpeake <msmcpeake@gmail.com>
2025-03-31 13:49:21 +0000

    Fix LDAP auth crash, switch to SCOPE_SUBTREE, and improve logging
    - Replaced unsafe `if out[0]:` with `if out:` to prevent IndexError on empty LDAP results
    - Changed LDAP search scope from SCOPE_BASE to SCOPE_SUBTREE to allow proper user lookup
    - Added debug logging for formatted LDAP filter, search results, and bind DNs
    - Improved admin group matching logic and encoding
    - Cleaned up comments for clarity and maintainability

--- app/server/fireshare/auth.py
+++ app/server/fireshare/auth.py
@@ -6,31 +6,46 @@ from .models import User
 from . import db
 import ldap
 import logging
+
 def auth_user_ldap(username, password):
-    formatted = current_app.config["LDAP_USER_FILTER"].format(input=username, basedn=current_app.config["LDAP_BASEDN"])
+    formatted = current_app.config["LDAP_USER_FILTER"].format(
+        input=username,
+        basedn=current_app.config["LDAP_BASEDN"]
+    )
     current_app.logger.debug("authenticating %s", username)
     current_app.logger.debug("formatted LDAP query: %s", formatted)
+    
     try:
-        # out is array of outputs, should only be one in this case
-        # each output is an array with index 0 being the dn and index 1 being the attrs
-        out = current_app.ldap_conn.search_ext_s(current_app.config["LDAP_BASEDN"],ldap.SCOPE_BASE, filterstr=formatted, attrlist=['memberOf'])
-        if out[0]:
+        out = current_app.ldap_conn.search_ext_s(
+            current_app.config["LDAP_BASEDN"],
+            ldap.SCOPE_SUBTREE,
+            filterstr=formatted,
+            attrlist=['memberOf']
+        )
+        current_app.logger.debug("LDAP search result: %s", out)
+
+        if out:
             dn = out[0][0]
             attrs = out[0][1]
             admin = True
-            if attrs and 'memberOf' in attrs.keys() and current_app.config["LDAP_ADMIN_GROUP"]:
-                admin_str = '{},{}'.format(current_app.config["LDAP_ADMIN_GROUP"], current_app.config["LDAP_BASEDN"]).encode()
-                current_app.logger.debug("matching against admin")
-                current_app.logger.debug(admin_str)
+
+            if attrs and 'memberOf' in attrs and current_app.config["LDAP_ADMIN_GROUP"]:
+                admin_str = '{},{}'.format(
+                    current_app.config["LDAP_ADMIN_GROUP"],
+                    current_app.config["LDAP_BASEDN"]
+                ).encode()
+                current_app.logger.debug("matching against admin group: %s", admin_str)
                 if admin_str in attrs['memberOf']:
                     current_app.logger.debug("matched admin")
                     admin = True
                 else:
                     current_app.logger.debug("matched not admin")
                     admin = False
+
             current_app.logger.debug("user search yielded result")
+
             conn2 = ldap.initialize(current_app.config["LDAP_URL"])
-            current_app.logger.debug("checking")
+            current_app.logger.debug("checking credentials")
             try:
                 conn2.bind_s(dn, password)
                 current_app.logger.debug("authorized user")
@@ -42,10 +57,12 @@ def auth_user_ldap(username, password):
         else:
             current_app.logger.debug("user search yielded no results")
             return False, False
+
     except:
         logging.exception('')
         current_app.logger.debug("failure at block1")
         return False, False
+
     return False, False
 
 
@@ -77,7 +94,7 @@ def login():
                 db.session.commit()
             login_user(userobj, remember=True)
             return Response(status=200)
-    
+
     return Response(response="Invalid username or password", status=401)
 
 @auth.route('/api/signup', methods=['POST'])
@@ -90,9 +107,8 @@ def signup():
     
     if user:
         return Response(response="User already exists.", status=400)
-    
-    new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
 
+    new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
     db.session.add(new_user)
     db.session.commit()
 
