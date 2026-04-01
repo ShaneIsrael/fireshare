1f4aaa36e4ea84b64cfe7becbd8d208250f1768b
dammitjeff <44111923+dammitjeff@users.noreply.github.com>
2026-04-01 12:36:19 -0700

    add adjustments to search bar design, fixed appearance on mobile

--- app/client/src/components/search/Search.js
+++ app/client/src/components/search/Search.js
@@ -3,7 +3,7 @@ import { Box, InputAdornment, TextField } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 import { useDebounce } from '../../common/utils'
 
-const Search = ({ searchHandler, placeholder, sx }) => {
+const Search = ({ searchHandler, placeholder, sx, autoFocus }) => {
   const [search, setSearch] = React.useState('')
   const debouncedSearch = useDebounce(search, 500)
 
@@ -13,16 +13,25 @@ const Search = ({ searchHandler, placeholder, sx }) => {
     <Box {...sx}>
       <TextField
         size="small"
-        // variant="standard"
         fullWidth
+        autoFocus={autoFocus}
         placeholder={placeholder}
         onChange={(e) => setSearch(e.target.value)}
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
-              <SearchIcon />
+              <SearchIcon sx={{ color: '#FFFFFF60' }} />
             </InputAdornment>
           ),
+          sx: {
+            borderRadius: '8px',
+            bgcolor: '#FFFFFF0D',
+            color: 'white',
+            '& fieldset': { borderColor: '#FFFFFF26' },
+            '&:hover fieldset': { borderColor: '#FFFFFF55' },
+            '&.Mui-focused fieldset': { borderColor: '#3399FF' },
+            '& input::placeholder': { color: '#FFFFFF60', opacity: 1 },
+          },
         }}
       />
     </Box>
